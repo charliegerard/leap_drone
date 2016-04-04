d42fae18f1a3ed8c01f30fcc9412779f399917ae
Charlie gerard <annecharlotte.gerard@gmail.com>
2016-04-04 17:49:30 +1000

    minor changes

--- README.md
+++ README.md
@@ -1,35 +1,28 @@
-Description
-=======================
+# Control a Parrot AR Drone 2.0 using a Leap Motion in Node.js.
 
-Control a Parrot AR Drone 2.0 using a Leap Motion.
+# How to Fly
+---
 
-How to Fly
-=======================
+*(TODO: Need to improve commands. May be really sensitive.)*
 
-(TODO: Need to improve commands. May be really sensitive.)
+At the moment, **speed is not activated** for indoor demos so drone just takes off and lands.
+To activate speed, go into the leap.js file in the public folder and comment out the `speed` argument passed in the directions functions.
 
 1. To takeoff, hold hand over Leap Motion.
 2. Keeping fingers together, move hand right to move drone right, left to move drone left, up to move drone up, down to move drone down, forward to move drone forward and backward to move drone backwards.
 3. To rotate, make a circle with pointer finger in a clockwise or counterclockwise motion.
 4. To land, remove hand.
 
-Stack
-=======================
+# Stack
+---
 
-Node.js for server
-Express for web app deployment
-Faye for publishing and subscribing between leap, server and drone
-Leap.js for converting leap motions into javascript
-jQuery for browser displays
+* Node.js for server
+* Express for web app deployment
+* Faye for publishing and subscribing between leap, server and drone
+* Leap.js for converting leap motions into javascript
+* jQuery for browser displays
 
-Learn More
-=======================
+# Learn More
+---
 
 I also recreated the same project using the Cylon.js framework. You can find a tutorial for this here: https://charliegerard.wordpress.com/2015/01/20/drone-leap-motion-cylon-js/
-
-Next steps
-=======================
-
-Improve the commands to make the 'stop' functionality work.
-Change the gesture to take off and land.
-Pair with the Myo armband.
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-  var express, path, drone, server, app, faye, client, controller, morgan, methodOverride, Leap;
+var express, path, drone, server, app, faye, client, controller, morgan, methodOverride, Leap;
 
-  express = require("express");
-  morgan = require("morgan");
-  bodyParser = require("body-parser");
-  methodOverride = require('method-override');
-  path = require("path");
-  http = require('http');
-  faye = require('faye');
-  Leap = require('leapjs');
-  drone = require("ar-drone").createClient(); // enables communication with drone in javascript
-  require("dronestream").listen(3001); // for video rendering
-  app = express();
+express = require("express");
+morgan = require("morgan");
+bodyParser = require("body-parser");
+methodOverride = require('method-override');
+path = require("path");
+http = require('http');
+faye = require('faye');
+Leap = require('leapjs');
+drone = require("ar-drone").createClient(); // enables communication with drone in javascript
+require("dronestream").listen(3001); // for video rendering
+app = express();
 
 var env = process.env.NODE_ENV || 'development';
 if('development' == env){
@@ -19,29 +19,29 @@ if('development' == env){
   app.use("/node_modules", express.static(path.join(__dirname, 'node_modules')))
 }
 
-  server = require('http').createServer(app);
+server = require('http').createServer(app);
 
-  var bayeux = new faye.NodeAdapter({  // central messaging server for clients to communicate with one another; Can also add 'engine' property, which controls backend of the server (ie faye-redis) and 'ping' property, which is how often, in seconds, to send keep-alive ping messages over WebSocket and EventSource connections. Used if Faye server will be accessed through a proxy that kills idle connections.
-    mount: '/faye', // path on the host at which the Faye service is available (ie http://localhost:3001/faye).
-    timeout: 50  // maximum time to hold connection open before returning response. Given in seconds and must be smaller than timeout on frontend webserver.
-  });
+var bayeux = new faye.NodeAdapter({  // central messaging server for clients to communicate with one another; Can also add 'engine' property, which controls backend of the server (ie faye-redis) and 'ping' property, which is how often, in seconds, to send keep-alive ping messages over WebSocket and EventSource connections. Used if Faye server will be accessed through a proxy that kills idle connections.
+  mount: '/faye', // path on the host at which the Faye service is available (ie http://localhost:3001/faye).
+  timeout: 50  // maximum time to hold connection open before returning response. Given in seconds and must be smaller than timeout on frontend webserver.
+});
 
-  bayeux.attach(server); // attached to server; will handle all requests to paths matching the mount path and delegate all other requests to handlers.
+bayeux.attach(server); // attached to server; will handle all requests to paths matching the mount path and delegate all other requests to handlers.
 
-  client = new faye.Client("http://localhost:" + (app.get("port")) + "/faye", {}); // sets up new client at environmental port that accesses the server at the /faye mount
+client = new faye.Client("http://localhost:" + (app.get("port")) + "/faye", {}); // sets up new client at environmental port that accesses the server at the /faye mount
 
-  client.subscribe("/drone/move", function (d) { // move includes any directional actions
-    console.log(d);
-    console.log("drone move?")
-    return drone[d.action](d.speed);
-  });
+client.subscribe("/drone/move", function (d) { // move includes any directional actions
+  console.log(d);
+  console.log("drone move?")
+  return drone[d.action](d.speed);
+});
 
-  client.subscribe("/drone/drone", function (d) { // drone commands include takeoff and landing
-    console.log(d);
-    console.log("drone stuff")
-      return drone[d.action]();
-  });
+client.subscribe("/drone/drone", function (d) { // drone commands include takeoff and landing
+  console.log(d);
+  console.log("drone stuff")
+  return drone[d.action]();
+});
 
-  server.listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get("port"));
-  })
+server.listen(app.get('port'), function () {
+  console.log("Express server listening on port " + app.get("port"));
+})
